79be1bf71c56a419cfd1536cf46c78641d257c0a
Hoàng Đức Long <nothing.noonedr@gmail.com>
2017-05-07 23:05:16 +0700

    first

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public Animator anim;
+	public Rigidbody2D rigid;
+	public bool isRunning = true;
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		if (isRunning && Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetTrigger ("Jump");
+			isRunning = false;
+			rigid.AddForce (new Vector2 (0, 300f));
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D hit)
+	{
+		if (!isRunning && hit.transform.CompareTag ("Land")) {
+			anim.SetTrigger ("Landing");
+			isRunning = true;
+		}
+	}
+}
